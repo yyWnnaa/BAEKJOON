528b1c3b2344d166600803f617aed568ba00058a
박규민 <102893980+yyWnnaa@users.noreply.github.com>
2024-02-03 12:28:12 +0900

    Baekjoon 4299 python

--- Baekjoon04299.py
+++ Baekjoon04299.py
@@ -2,7 +2,17 @@ import sys
 
 hap, cha = map(int, sys.stdin.readline().split())
 
-winS = (hap + cha) // 2
-loseS = hap - winS
-
-sys.stdout.write(str(winS) + ' ' + str(loseS))
+if hap - cha < 0 or (hap - cha) % 2 != 0:
+#  hap + cha < 0 으로 바꾸면 틀림
+#                   (hap + cha) % 2 != 0: 으로 바꿔도 맞음
+    sys.stdout.write(str(-1))
+else:
+    winS = (hap + cha) // 2
+    loseS = hap - winS
+    
+    #아래와 같이 하지 않고 max min 함수를 사용해도 된다.
+    if loseS > winS:
+        temp = loseS
+        loseS = winS
+        winS = temp
+    sys.stdout.write(str(winS) + ' ' + str(loseS))
